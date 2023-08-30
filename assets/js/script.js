467857df89ff497b99282cfab34d1dfa0f5e7088
Andrii Tsokur <tsokur@outlook.com>
2023-08-30 19:03:32 +0300

    feat: Saves UI theme to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,26 @@
+const LOCAL_STORAGE_KEY = 'ui-theme';
 const refs = {
 	aside: document.querySelectorAll('aside'),
 	socialList: document.querySelector('.social__list'),
+	mainStyles: document.querySelector('link[data-anchor="main-styles"]'),
 	darkModeBtn: document.querySelector('.main-nav__link--dark-mode'),
 	navButtons: [...document.querySelectorAll('.main-nav__link')],
 	mainSections: document.querySelectorAll('main > section'),
 	eduNworkBlocks: document.querySelectorAll('.edu-n-work__wrapper'),
 };
-
 refs.navButtons.shift();
+
+console.log(refs.mainStyles);
+
+if (!localStorage.getItem(LOCAL_STORAGE_KEY)) {
+	localStorage.setItem(LOCAL_STORAGE_KEY, 'light');
+} else {
+	if (localStorage.getItem(LOCAL_STORAGE_KEY) === 'dark') {
+		activateDarkMode();
+	} else {
+		refs.mainStyles.nextElementSibling?.remove();
+	}
+}
 
 window.addEventListener(
 	'scroll',
@@ -46,22 +59,28 @@ function showActiveSection() {
 }
 
 function activateDarkMode(e) {
-	e.preventDefault();
+	e?.preventDefault();
 
 	if (refs.darkModeBtn.classList.contains('main-nav__link--current')) {
-		document.head.lastChild.remove();
+		refs.mainStyles.nextElementSibling.remove();
+		localStorage.setItem(LOCAL_STORAGE_KEY, 'light');
 	} else {
 		const darkModeStyle = document.createElement('link');
 		darkModeStyle.rel = 'stylesheet';
 		darkModeStyle.href = './assets/css/dark-mode.css';
-		document.head.append(darkModeStyle);
+		refs.mainStyles.after(darkModeStyle);
+		localStorage.setItem(LOCAL_STORAGE_KEY, 'dark');
 	}
 
 	refs.darkModeBtn.classList.toggle('main-nav__link--current');
-	removeFocus(e);
+	if (e) removeFocus(e);
 }
 
 function removeFocus(e) {
 	e.target.blur();
 	e.target.parentNode.blur();
+}
+
+function handleStorage() {
+	localStorage.getItem('ui-theme');
 }
